445b237231a2dad719136bd28302c2e3dd0cea46
Chris Rittenhouse <dev@litritt.com>
2023-03-12 12:07:02 -0400

    Begin adding menu item for Skin Swap

--- Delta/Pause Menu/PauseViewController.swift
+++ Delta/Pause Menu/PauseViewController.swift
@@ -19,7 +19,7 @@ class PauseViewController: UIViewController, PauseInfoProviding
     }
     
     var pauseItems: [MenuItem] {
-        return [self.saveStateItem, self.loadStateItem, self.cheatCodesItem, self.fastForwardItem, self.sustainButtonsItem, self.rewindItem].compactMap { $0 }
+        return [self.saveStateItem, self.loadStateItem, self.cheatCodesItem, self.fastForwardItem, self.sustainButtonsItem, self.rewindItem, self.skinSwapItem].compactMap { $0 }
     }
     
     /// Pause Items
@@ -29,6 +29,7 @@ class PauseViewController: UIViewController, PauseInfoProviding
     var fastForwardItem: MenuItem?
     var sustainButtonsItem: MenuItem?
     var rewindItem: MenuItem?
+    var skinSwapItem: MenuItem?
     
     /// PauseInfoProviding
     var pauseText: String?
@@ -188,6 +189,7 @@ private extension PauseViewController
         
         self.fastForwardItem = MenuItem(text: NSLocalizedString("Fast Forward", comment: ""), image: #imageLiteral(resourceName: "FastForward"), action: { _ in })
         self.sustainButtonsItem = MenuItem(text: NSLocalizedString("Hold Buttons", comment: ""), image: #imageLiteral(resourceName: "SustainButtons"), action: { _ in })
+        self.skinSwapItem = MenuItem(text: NSLocalizedString("Swap Skin", comment: ""), image: #imageLiteral(resourceName: "SkinSwap"), action: { _ in })
     }
     
     func updateSafeAreaInsets()
