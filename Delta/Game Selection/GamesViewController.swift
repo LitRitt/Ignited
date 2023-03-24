3d233ee2205c60246d8e507b68acc41a4a0f4d4f
Chris Rittenhouse <dev@litritt.com>
2023-03-24 17:23:32 -0400

    Remove unneeded Deltroid commit

--- Delta/Game Selection/GamesViewController.swift
+++ Delta/Game Selection/GamesViewController.swift
@@ -385,8 +385,6 @@ extension GamesViewController: ImportControllerDelegate
         
         // .bin files (Genesis ROMs)
         documentTypes.insert("com.apple.macbinary-archive")
-        documentTypes.insert("public.plain-text")
-        documentTypes.insert("net.daringfireball.markdown")
         
         // Add GBA4iOS's exported UTIs in case user has GBA4iOS installed (which may override Delta's UTI declarations)
         documentTypes.insert("com.rileytestut.gba")
