47673d12b193567c1f2e138c4827ed59fd64b375
Chris Rittenhouse <dev@litritt.com>
2023-03-24 17:24:51 -0400

    UIImage and UIColor extensions
    - Adjust brightness of a UIColor
    - Apply a UIFilter to a UIImage

---
+++ Delta/Extensions/UIColor+Adjust.swift
@@ -0,0 +1,53 @@
+//
+//  UIColor+Adjust.swift
+//  Delta
+//
+//  Created by Chris Rittenhouse on 3/23/23.
+//  Copyright © 2023 Lit Development. All rights reserved.
+//
+//  From https://www.advancedswift.com/lighter-and-darker-uicolor-swift/
+
+import UIKit
+
+extension UIColor
+{
+    private func makeColor(componentDelta: CGFloat) -> UIColor {
+        var red: CGFloat = 0
+        var blue: CGFloat = 0
+        var green: CGFloat = 0
+        var alpha: CGFloat = 0
+        
+        // Extract r,g,b,a components from the
+        // current UIColor
+        getRed(
+            &red,
+            green: &green,
+            blue: &blue,
+            alpha: &alpha
+        )
+        
+        // Create a new UIColor modifying each component
+        // by componentDelta, making the new UIColor either
+        // lighter or darker.
+        return UIColor(
+            red: add(componentDelta, toComponent: red),
+            green: add(componentDelta, toComponent: green),
+            blue: add(componentDelta, toComponent: blue),
+            alpha: alpha
+        )
+    }
+
+    // Add value to component ensuring the result is
+    // between 0 and 1
+    private func add(_ value: CGFloat, toComponent: CGFloat) -> CGFloat {
+        return max(0, min(1, toComponent + value))
+    }
+    
+    func lighter(componentDelta: CGFloat = 0.1) -> UIColor {
+        return makeColor(componentDelta: componentDelta)
+    }
+
+    func darker(componentDelta: CGFloat = 0.1) -> UIColor {
+        return makeColor(componentDelta: -1*componentDelta)
+    }
+}
