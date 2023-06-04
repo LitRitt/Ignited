b17ba42028a493e92d4d6ad91d807d2baa104c5f
Chris Rittenhouse <dev@litritt.com>
2023-06-03 22:55:35 -0400

    Reorganize project files

--- Delta/Features/GamesCollection/GameArtwork.swift
+++ Delta/Features/GamesCollection/GameArtwork.swift
@@ -87,63 +87,6 @@ struct GameArtworkOptions
     })
     var bgOpacity: Double = 0.7
     
-    @Option(name: "Favorite Games First",
-            description: "Sort favorited games to the top of the games list.")
-    var favoriteSort: Bool = true
-    
-    @Option(name: "Highlight Favorite Games",
-            description: "Give your favorite games a distinct glow.")
-    var favoriteHighlight: Bool = true
-    
-    @Option(name: "Favorite Highlight Color",
-            description: "Select a custom color to use to highlight your favorite games.",
-            detailView: { value in
-        ColorPicker("Favorite Highlight Color", selection: value, supportsOpacity: false)
-            .displayInline()
-    })
-    var favoriteColor: Color = Color(red: 255/255, green: 234/255, blue: 0/255)
-    
-    @Option(name: "Animation Speed", description: "Adjust the animation speed of animated artwork.", detailView: { value in
-        VStack {
-            HStack {
-                Text("Animation Speed: \(value.wrappedValue * 100, specifier: "%.f")%")
-                Spacer()
-            }
-            HStack {
-                Text("50%")
-                Slider(value: value, in: 0.5...2.0, step: 0.05)
-                Text("200%")
-            }
-        }.displayInline()
-    })
-    var animationSpeed: Double = 1.0
-    
-    @Option(name: "Animation Delay", description: "Adjust the pause between animations for animated artwork.", detailView: { value in
-        VStack {
-            HStack {
-                Text("Animation Pause: \(value.wrappedValue, specifier: "%.f") Frames")
-                Spacer()
-            }
-            HStack {
-                Text("0")
-                Slider(value: value, in: 0...20, step: 1)
-                Text("20")
-            }
-        }.displayInline()
-    })
-    var animationPause: Double = 0
-    
-    @Option
-    var favoriteGames: [String: [String]] = [
-        System.ds.gameType.rawValue: [],
-        System.gba.gameType.rawValue: [],
-        System.gbc.gameType.rawValue: [],
-        System.nes.gameType.rawValue: [],
-        System.snes.gameType.rawValue: [],
-        System.n64.gameType.rawValue: [],
-        System.genesis.gameType.rawValue: []
-    ]
-    
     @Option(name: "Title Size", description: "The size of the game's title.", detailView: { value in
         VStack {
             HStack {
