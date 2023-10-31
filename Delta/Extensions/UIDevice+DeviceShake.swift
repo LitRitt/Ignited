f26a5835b981284e94138f926265e675c5c84b7d
Chris Rittenhouse <dev@litritt.com>
2023-10-31 14:08:44 -0400

    Posts to notification center when device is shaken

---
+++ Delta/Extensions/UIDevice+DeviceShake.swift
@@ -0,0 +1,14 @@
+//
+//  UIDevice+DeviceShake.swift
+//  Delta
+//
+//  Created by Chris Rittenhouse on 10/31/23.
+//  Copyright © 2023 LitRitt. All rights reserved.
+//
+
+import UIKit
+
+extension UIDevice
+{
+    static let deviceDidShakeNotification = Notification.Name(rawValue: "deviceDidShakeNotification")
+}
---
+++ Delta/Extensions/UIWindow+DeviceShake.swift
@@ -0,0 +1,21 @@
+//
+//  UIWindow+DeviceShake.swift
+//  Delta
+//
+//  Created by Chris Rittenhouse on 10/31/23.
+//  Copyright © 2023 LitRitt. All rights reserved.
+//
+
+import UIKit
+
+extension UIWindow
+{
+     open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?)
+    {
+        switch motion
+        {
+        case .motionShake: NotificationCenter.default.post(name: UIDevice.deviceDidShakeNotification, object: nil)
+        default: break
+        }
+    }
+}
