146d29df589d98aee7b65198a00b4d61c2965ad0
Chris Rittenhouse <dev@litritt.com>
2023-12-06 14:57:24 -0500

    Makes custom artwork background color support transparency

--- Ignited/Features/Library/GameArtworkOptions.swift
+++ Ignited/Features/Library/GameArtworkOptions.swift
@@ -218,7 +218,7 @@ struct GameArtworkOptions
     
     @Option(name: "Custom Background Color",
             detailView: { value in
-        ColorPicker("Custom Background Color", selection: value, supportsOpacity: false)
+        ColorPicker("Custom Background Color", selection: value, supportsOpacity: true)
             .displayInline()
     })
     var backgroundColor: Color = .orange
