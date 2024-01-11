6cf324ad65dfbf4a945374c38949396d0f4742cf
Topher Rittenhouse <dev@litritt.com>
2024-01-10 20:38:29 -0500

    Adds view to see all OpenGLES 3 games and remove them

--- Ignited/Features/Cores/N64/N64OpenGLES3Options.swift
+++ Ignited/Features/Cores/N64/N64OpenGLES3Options.swift
@@ -15,6 +15,16 @@ struct N64OpenGLES3Options
     @Option
     var enabledGames: [String] = []
     
+    @Option(name: "View Enabled Games", description: "View Enabled Games", detailView: { _ in
+        List {
+            ForEach(getEnabledGames(), id: \.self) { game in
+                Text(game)
+            }
+            .onDelete(perform: removeGame)
+        }
+    })
+    var viewEnabledGames: String = ""
+    
     @Option(name: "Reset Enabled Games",
             description: "Disable OpenGLES 3 for all games.",
             detailView: { _ in
@@ -26,4 +36,36 @@ struct N64OpenGLES3Options
         .displayInline()
     })
     var reset: Bool = false
+}
+
+extension N64OpenGLES3Options
+{
+    static func removeGame(at offsets: IndexSet)
+    {
+        Settings.n64Features.openGLES3.enabledGames.remove(atOffsets: offsets)
+    }
+    
+    static func getEnabledGames() -> [String]
+    {
+        var games: [String] = []
+        
+        let gameFetchRequest = Game.rst_fetchRequest() as! NSFetchRequest<Game>
+        gameFetchRequest.predicate = NSPredicate(format: "%K IN %@", #keyPath(Game.identifier), Settings.n64Features.openGLES3.enabledGames)
+        
+        do
+        {
+            let enabledGames = try DatabaseManager.shared.viewContext.fetch(gameFetchRequest)
+            
+            for game in enabledGames
+            {
+                games.append(game.name)
+            }
+        }
+        catch
+        {
+            print(error)
+        }
+        
+        return games
+    }
 }
