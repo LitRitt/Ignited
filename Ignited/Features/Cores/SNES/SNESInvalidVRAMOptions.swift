fdad4d39c5ee4c2e49f87fbe942efc37a713c66e
Topher Rittenhouse <dev@litritt.com>
2024-01-10 20:38:42 -0500

    Adds view to see all Invalid VRAM games and remove them

--- Ignited/Features/Cores/SNES/SNESInvalidVRAMOptions.swift
+++ Ignited/Features/Cores/SNES/SNESInvalidVRAMOptions.swift
@@ -15,6 +15,16 @@ struct SNESInvalidVRAMOptions
     @Option
     var enabledGames: [String] = []
     
+    @Option(name: "View Enabled Games", description: "View Enabled Games", detailView: { _ in
+        List {
+            ForEach(getEnabledGames(), id: \.self) { game in
+                Text(game)
+            }
+            .onDelete(perform: removeGame)
+        }
+    })
+    var viewEnabledGames: String = ""
+    
     @Option(name: "Reset Enabled Games",
             description: "Disable Invalid VRAM Access for all games.",
             detailView: { _ in
@@ -26,4 +36,36 @@ struct SNESInvalidVRAMOptions
         .displayInline()
     })
     var reset: Bool = false
+}
+
+extension SNESInvalidVRAMOptions
+{
+    static func removeGame(at offsets: IndexSet)
+    {
+        Settings.snesFeatures.allowInvalidVRAMAccess.enabledGames.remove(atOffsets: offsets)
+    }
+    
+    static func getEnabledGames() -> [String]
+    {
+        var games: [String] = []
+        
+        let gameFetchRequest = Game.rst_fetchRequest() as! NSFetchRequest<Game>
+        gameFetchRequest.predicate = NSPredicate(format: "%K IN %@", #keyPath(Game.identifier), Settings.snesFeatures.allowInvalidVRAMAccess.enabledGames)
+        
+        do
+        {
+            let enabledGames = try DatabaseManager.shared.viewContext.fetch(gameFetchRequest)
+            
+            for game in enabledGames
+            {
+                games.append(game.name)
+            }
+        }
+        catch
+        {
+            print(error)
+        }
+        
+        return games
+    }
 }
