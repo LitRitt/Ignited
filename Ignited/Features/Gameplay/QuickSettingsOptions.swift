8e256a8c087726b8216f7cd4f07fb4245e8d9623
Topher Rittenhouse <dev@litritt.com>
2024-02-01 13:32:47 -0500

    Makes the quick settings button replacement free

--- Ignited/Features/Gameplay/QuickSettingsOptions.swift
+++ Ignited/Features/Gameplay/QuickSettingsOptions.swift
@@ -44,8 +44,7 @@ struct QuickSettingsOptions
     
     @Option(name: "Replace Button",
             description: "Choose an input, like fast forward or screenshot, to use with the quick settings button on skins instead of opening the quick settings menu.",
-            values: [ActionInput.fastForward, ActionInput.quickSave, ActionInput.quickLoad, ActionInput.screenshot, ActionInput.restart],
-            attributes: [.pro])
+            values: [ActionInput.fastForward, ActionInput.quickSave, ActionInput.quickLoad, ActionInput.screenshot, ActionInput.restart])
     var buttonReplacement: ActionInput? = nil
     
     @Option(name: "Quick Actions",
