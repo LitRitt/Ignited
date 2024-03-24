73a54e979c8ab6b542a425e75621a37904cc8944
Topher Rittenhouse <dev@litritt.com>
2024-03-24 12:38:47 -0400

    [mGBA] Adds idle optimization for GBA

--- Ignited/Features/Cores/GBA/MGBAOptions.swift
+++ Ignited/Features/Cores/GBA/MGBAOptions.swift
@@ -21,4 +21,9 @@ struct MGBAOptions
             description: "Choose how much frames should be skipped to improve performance at the expense of visual smoothness.",
             values: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     var frameskip: Int32 = 0
+    
+    @Option(name: "Idle Loop Removal",
+            description: "Optimizes game performance by driving the GBA's CPU less hard. Use this on low-powered hardware if its struggling with game performance.",
+            values: GBIdleOptimization.allCases)
+    var idleOptimization: GBIdleOptimization = .remove
 }
