32ab29f205b0148f83106056501f2fe93de01c8f
Topher Rittenhouse <dev@litritt.com>
2024-07-25 21:32:52 -0400

    [Project] Fixes missing patreon file

---
+++ Ignited/Patreon/PatreonSecrets.swift
@@ -0,0 +1,19 @@
+//
+//  PatreonSecrets.swift
+//  Ignited
+//
+//  Created by Chris Rittenhouse on 7/18/24.
+//  Copyright © 2024 LitRitt. All rights reserved.
+//
+
+import Foundation
+
+class PatreonSecrets
+{
+    static let shared = PatreonSecrets()
+    
+    let patreonCreatorAccessToken: String = ""
+    
+    required init() {
+    }
+}
